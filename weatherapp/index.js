d44b2ededbfda929ec940bd363b472b4c2cc03fa
Abhi067852 <abhishek067852@gmail.com>
2020-10-25 13:27:44 +0530

    weather-app

--- weatherapp/index.js
+++ weatherapp/index.js
@@ -1,9 +1,29 @@
+const bodyParser = require("body-parser");
 const express = require("express");
-const { STATUS_CODES } = require("http");
+
 const https = require("https");
+
+
+
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=New Delhi&units=metric&appid=89530341787ff181c20f7631c3ab103b";
+
+
+    //     })
+    //     // res.send("I am Working all good ");
+    res.sendFile(__dirname + "/index.html");
+    // res.send("IAm working");
+
+}
+)
+app.post("/", function (req, res) {
+    console.log("Iam working");
+    console.log(req.body.cityName);
+    const query = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=89530341787ff181c20f7631c3ab103b";
     https.get(url, function (response) {
         console.log(response.statusCode);
         response.on("data", function (data) {
@@ -16,12 +36,11 @@ app.get("/", function (req, res) {
             console.log(temp);
             console.log(weather);
             res.write("<img src=" + imgurl + ">");
-            res.write("<h1>Temperature in New Delhi is " + temp + " degree celsius</h1 >");
+            res.write('<h1>Temperature in ' + query + ' is ' + temp + ' degree celsius</h1>');
             res.send();
-        })
+        });
 
-    })
-    // res.send("I am Working all good ");
+    });
 })
 
 
@@ -30,6 +49,7 @@ app.get("/", function (req, res) {
 
 
 
-app.listen(3000, function () {
+
+app.listen(3001, function () {
     console.log("server is listen");
 })
